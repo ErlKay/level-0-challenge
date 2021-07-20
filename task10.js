a33a923dd855de962d4bc33785d1681a2f2f8197
Kgotlello <motsepekgotlello123@gmail.com>
2021-07-20 19:14:40 +0200

    task 10

---
+++ task10.js
@@ -0,0 +1,12 @@
+function common(str1, str2) {
+    let sameAlph = '';
+    for (let i = 0; i < str1.length; i += 1) {
+      if(sameAlph.indexOf(str1[i]) === -1) {
+        if (str2.indexOf(str1[i]) !== -1) {
+          sameAlph += str1[i];
+        }
+      }
+    }
+    return sameAlph;
+  }
+  console.log(common('house', 'computers'));
