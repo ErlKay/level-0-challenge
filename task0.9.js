eeaebc517f49480ec083cc351446320af576bf49
ErlKay <67595055+ErlKay@users.noreply.github.com>
2021-07-22 15:19:10 +0200

    Update task0.9.js

--- task0.9.js
+++ task0.9.js
@@ -1,10 +1,12 @@
-const stringToCheck = 'Umuzi';
-const consonant = ['m', 'z'];
-let newString = ' ';
-
-for(const vowels of stringToCheck) {
-  if(consonant.indexOf(vowels) === -1) {
-    newString += vowels;
+function findVowels() {
+    var vowels = ['a', 'e', 'i', 'o', 'u'];
+    var vowelsFound = [];
+    var word = 'Umuzi';
+    for(k = 0; k < word.length; k++) {
+      if(vowels.indexOf(word[k]) !== -1) {
+        vowelsFound.push(word[k]);
+      }
+    }
+    return vowelsFound;
   }
-}
-console.log(newString);
+  
